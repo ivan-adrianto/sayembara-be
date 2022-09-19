5b5f1255ec81857252f38de58d7bc70794243757
Ivan Adrianto <ivanadrianto64@gmail.com>
2022-09-19 15:45:22 +0700

    add join_status and status in detail contest

--- routes/handler/contests/getContestDetail.js
+++ routes/handler/contests/getContestDetail.js
@@ -8,7 +8,7 @@ contest.hasOne(User, {
   foreignKey: "id",
   sourceKey: "provider_id",
 });
-const { Op } = require("sequelize")
+const { Op } = require("sequelize");
 
 module.exports = async (req, res) => {
   const id = req.params.id;
@@ -24,6 +24,7 @@ module.exports = async (req, res) => {
         "description",
         "due_date",
         "announcement_date",
+        "winner_id",
       ],
     });
 
@@ -32,9 +33,19 @@ module.exports = async (req, res) => {
       attributes: ["id", "thumbnail", "title", "description"],
     });
     const othersSubmissions = await submission.findAll({
-      where: { contest_id: id, participant_id: {[Op.not]: req.user.id} },
+      where: { contest_id: id, participant_id: { [Op.not]: req.user.id } },
       attributes: ["id", "thumbnail", "title", "description"],
     });
+    const joinStatus = () => {
+      if (contests.winner_id === req.user.id) {
+        return "winner";
+      }
+      if (mySubmissions.length > 0) {
+        return "joined";
+      }
+      return "not join";
+    };
+
     const data = {
       ...contests.dataValues,
       submissions: [...mySubmissions, ...othersSubmissions],
@@ -42,8 +53,14 @@ module.exports = async (req, res) => {
       posted_since: timeDiff(contests.dataValues.created_at),
       due_date: formatDate(contests.dataValues.due_date),
       announcement_date: formatDate(contests.dataValues.announcement_date),
-      status: new Date() < new Date(contests.dataValues.due_date) ? "Open" : "Closed",
+      status:
+        new Date() < new Date(contests.dataValues.due_date) &&
+        !contests.winner_id
+          ? "Open"
+          : "Closed",
+      join_status: joinStatus(),
     };
+    
     res.json({
       status: "success",
       data,
